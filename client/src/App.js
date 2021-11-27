eb51fd75396e63f0d0ad1bfdcb9cc6254d998bbe
ZnoGouDj <znogoud3@gmail.com>
2021-11-27 23:28:02 +0300

    Routing setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
 import 'materialize-css';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { useRoutes } from './routes';
 
 function App() {
+  const routes = useRoutes(false);
   return (
-    <div className="container">
-      <h1>Hello</h1>
-    </div>
+    <Router>
+      <div className="container">{routes}</div>
+    </Router>
   );
 }
 
