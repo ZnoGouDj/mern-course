0cd81d0c4e92364ee668fe5da2da35aaa32cd7a0
ZnoGouDj <znogoud3@gmail.com>
2021-11-27 21:44:55 +0300

    Routes registration added

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,16 +1,88 @@
 const { Router } = require('express');
+const bcrypt = require('bcryptjs');
+const config = require('config');
+const jwt = require('jsonwebtoken');
+const { check, validationResult } = require('express-validator');
+const User = require('../models/User');
 const router = Router();
 
 // /api/auth/register
-router.post('/register', async (req, res) => {
-  try {
-    const { email, password } = req.body;
-  } catch (e) {
-    res.status(500).json({ message: 'Something went terribly wrong, please try again' });
+router.post(
+  '/register',
+  [
+    check('email', 'Incorrect email').isEmail(),
+    check('password', 'Password should be longer than 6 symbols').isLength({ min: 6 }),
+  ],
+  async (req, res) => {
+    try {
+      const errors = validationResult(req);
+
+      if (!error.isEmpty()) {
+        return res.status(400).json({
+          errors: errors.array(),
+          message: 'Incorrect registration data',
+        });
+      }
+
+      const { email, password } = req.body;
+
+      const candidate = await User.findOne({ email });
+
+      if (candidate) {
+        return res.status(400).json({ message: 'This user has already been registered' });
+      }
+
+      const hashedPassword = await bcrypt.hash(password, 12);
+      const user = new User({ email, password: hashedPassword });
+
+      await user.save();
+
+      res.status(201).json({ message: 'User created' });
+    } catch (e) {
+      res.status(500).json({ message: 'Something went terribly wrong, please try again' });
+    }
   }
-});
+);
 
 // /api/auth/login
-router.post('/login', async (req, res) => {});
+router.post(
+  '/login',
+  [
+    check('email', 'Please, enter correct email').normalizeEmail().isEmail(),
+    check('password', 'Please, enter the password').exists(),
+  ],
+  async (req, res) => {
+    try {
+      const errors = validationResult(req);
+
+      if (!error.isEmpty()) {
+        return res.status(400).json({
+          errors: errors.array(),
+          message: 'Incorrect login data',
+        });
+      }
+
+      const { email, password } = req.body;
+
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        return res.status(400).json({ message: "User can't be found" });
+      }
+
+      const isMatch = await bcrypt.compare(password, user.password);
+
+      if (!isMatch) {
+        return res.status(400).json({ message: 'Incorrect password, please try again' });
+      }
+
+      const token = jwt.sign({ userId: user.id }, config.get('jwtSecret'), { expiresIn: '1h' });
+
+      res.json({ token, userId: user.id });
+    } catch (e) {
+      res.status(500).json({ message: 'Something went terribly wrong, please try again' });
+    }
+  }
+);
 
 module.exports = router;
