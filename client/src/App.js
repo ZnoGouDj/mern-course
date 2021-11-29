e5b4c5f7674e0bc6c185c02b56c48c76e6eb5e06
ZnoGouDj <znogoud3@gmail.com>
2021-11-29 15:12:25 +0300

    Navbar styles, functionality implementation

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import { useRoutes } from './routes';
 import { useAuth } from './hooks/auth.hook';
 import { AuthContext } from './context/AuthContext';
+import { Navbar } from './components/Navbar';
 
 function App() {
   const { token, login, logout, userId } = useAuth();
@@ -19,6 +20,7 @@ function App() {
       }}
     >
       <Router>
+        {isAuthenticated && <Navbar />}
         <div className="container">{routes}</div>
       </Router>
     </AuthContext.Provider>
